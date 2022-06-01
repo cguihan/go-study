e4c4ebef5077faa0f750a327fa935ffb69c1171f
cguihan <457888727@qq.com>
2022-06-01 03:59:33 +0000

    v1

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8081") // listen and serve on 0.0.0.0:8080
+
+	//v1
+	fmt.Println("graham")
+}
